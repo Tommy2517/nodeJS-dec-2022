b018d58582ab492ce12f16b82f0c985c322e8bcf
Tommy2517 <artem07stu@gmail.com>
2023-06-03 15:20:19 +0300

    lesson1-homework

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-const {lesson1} = require("./lessons/lesson_1-module-path-os-fs/lesson1");
+// const {lesson1} = require("./lessons/lesson_1-module-path-os-fs/lesson1");
+// lesson1();
 const fs = require("fs");
 const path = require("path");
-lesson1();
-console.log('hello')
+console.log('hello okten - homeWork');
+// ДЗ:
+//     Створіть папку
+// В тій папці створіть 5 папок і 5 файлів
+// І за допомогою модулю fs виведіть в консоль, чи це папка чи це файл
+//
+// FILE: {fileName}
+// FOLDER: {folderName}
+//
+// !руками нічого не робимо, все через f
+
+
+fs.mkdir(path.join(__dirname,'lessons',))
